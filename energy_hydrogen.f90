f13fb07ed31abe4700d9bbf0726393b3716d1b1a
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2021-01-25 23:52:53 +0100

    OK up to VMC

--- energy_hydrogen.f90
+++ energy_hydrogen.f90
@@ -11,6 +11,26 @@ program energy_hydrogen
      x(i) = -5.d0 + (i-1)*dx
   end do
 
+  do j=1,size(a)
+     ! TODO
+     print *, 'a = ', a(j), '    E = ', energy
+  end do
+
+end program energy_hydrogen
+
+program energy_hydrogen
+  implicit none
+  double precision, external :: e_loc, psi
+  double precision :: x(50), w, delta, energy, dx, r(3), a(6), norm
+  integer :: i, k, l, j
+
+  a = (/ 0.1d0, 0.2d0, 0.5d0, 1.d0, 1.5d0, 2.d0 /)
+
+  dx = 10.d0/(size(x)-1)
+  do i=1,size(x)
+     x(i) = -5.d0 + (i-1)*dx
+  end do
+
   delta = dx**3
 
   r(:) = 0.d0
--- energy_hydrogen.py
+++ energy_hydrogen.py
@@ -9,15 +9,15 @@ r = np.array([0.,0.,0.])
 for a in [0.1, 0.2, 0.5, 0.9, 1., 1.5, 2.]:
     E = 0.
     norm = 0.
-      for x in interval:
-          r[0] = x
-            for y in interval:
-                r[1] = y
-                  for z in interval:
-                      r[2] = z
-                      w = psi(a,r)
-                      w = w * w * delta
-                      E    += w * e_loc(a,r)
-                      norm += w 
+    for x in interval:
+        r[0] = x
+        for y in interval:
+            r[1] = y
+            for z in interval:
+                r[2] = z
+                w = psi(a,r)
+                w = w * w * delta
+                E    += w * e_loc(a,r)
+                norm += w 
     E = E / norm
     print(f"a = {a} \t E = {E}")
